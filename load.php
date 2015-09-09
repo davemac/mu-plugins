9c542c1c273bbf902efe5e4e95bc5d4a2d78a3b8
David McDonald <info@davidmcdonald.org>
2015-09-09 17:36:13 +1000

    ok

--- load.php
+++
@@ -1,4 +0,0 @@
-<?php // mu-plugins/load.php
-require WPMU_PLUGIN_DIR . '/remove-wp-menu/remove-wp-menu.php';
-require WPMU_PLUGIN_DIR . '/dmc-helpers/dmc-helpers.php';
-require WPMU_PLUGIN_DIR . '/user-switching/user-switching.php';
