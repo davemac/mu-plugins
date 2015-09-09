2b3eddc978c039fc977d251deebb02b5c79e7e57
David McDonald <info@davidmcdonald.org>
2015-09-09 17:33:26 +1000

    first commit

---
+++ load.php
@@ -0,0 +1,4 @@
+<?php // mu-plugins/load.php
+require WPMU_PLUGIN_DIR . '/remove-wp-menu/remove-wp-menu.php';
+require WPMU_PLUGIN_DIR . '/dmc-helpers/dmc-helpers.php';
+require WPMU_PLUGIN_DIR . '/user-switching/user-switching.php';
