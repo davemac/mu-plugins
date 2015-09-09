ddc1009ba06b2889dba64e58d8da3a7c274da6ba
David McDonald <info@davidmcdonald.org>
2015-09-09 17:37:31 +1000

    ready to go

---
+++ dmc-helpers/dmc-helpers.php
@@ -0,0 +1,184 @@
+<?php 
+/*
+Plugin Name: DMC Web Helpers
+Plugin URI: http://www.dmcweb.com.au
+Description: Common helper functions used on most projects using customised Reverie theme
+Version: 1.0
+Author: David McDonald
+Author URI: http://www.davidmcodnald.org
+License: GPLv2
+Copyright 2013  David McDonald (email : info@davidmcdonald.org, twitter : @davemac)
+*/
+
+// Slider
+
+add_action('init', 'register_dmc_slider' , 0 );
+
+function register_dmc_slider() {
+ 
+    $labels = array(
+        'name' => _x( 'Homepage Slider', 'post type general name' ),
+        'singular_name' => _x( 'Homepage Slider', 'post type singular name' ),
+        'add_new' => _x( 'Add New', 'Homepage Slider' ),
+        'add_new_item' => __( 'Add New Homepage Slider' ),
+        'edit_item' => __( 'Edit Homepage Slider' ),
+        'new_item' => __( 'New Homepage Slider' ),
+        'all_items' => __( 'All Homepage Sliders' ),
+        'view_item' => __( 'View Homepage Slider' ),
+        'search_items' => __( 'Search Homepage Sliders' ),
+        'not_found' =>  __( 'No Homepage Sliders found' ),
+        'not_found_in_trash' => __( 'No Homepage Sliders found in Trash' ),
+        'parent_item_colon' => '',
+        'menu_name' => 'Homepage Slider'
+    
+      );
+      $args = array(
+        'labels' => $labels,
+        'public' => true,
+        'query_var' => true,
+        'rewrite' => true,
+        'capability_type' => 'post',
+        // 'capabilities' => array(
+//          'publish_posts' => 'manage_options',
+//          'edit_posts' => 'manage_options',
+//          'edit_others_posts' => 'manage_options',
+//          'delete_posts' => 'manage_options',
+//          'delete_others_posts' => 'manage_options',
+//          'read_private_posts' => 'manage_options',
+//          'edit_post' => 'manage_options',
+//          'delete_post' => 'manage_options',
+//          'read_post' => 'manage_options',
+//      ),
+        'has_archive' => true,
+        'hierarchical' => false,
+        'menu_position' => 5,
+        'rewrite' => array( 'slug' => 'slider', 'with_front' => false ),
+        'supports' => array( 'title', 'editor', 'thumbnail' )
+      );
+     
+    register_post_type( 'dmc_slider' , $args );
+}
+
+function SliderContent(){
+
+    $args = array( 'post_type' => 'dmc_slider', 'order' => 'ASC', 'posts_per_page' => 6 );
+    $dmcslider = new WP_Query( $args );
+    
+    while ( $dmcslider->have_posts() ) : $dmcslider->the_post(); ?>
+
+        <?php 
+            $slider_link = get_field('dmc_slider_image_links_to');
+            if( $slider_link ) : 
+                foreach( $slider_link as $p ) :
+        ?>
+            <div>
+                <a href="<?php echo get_permalink( $p->ID ); ?>">
+                    <div class="row collapse">
+                        <div class="small-12 columns">
+                            <div class="slider-meta">
+                                <h1><?php the_title(); ?></h1>
+                                <?php the_content(); ?>
+                                <span class="button radius large">Find Out More</span>
+                            </div>
+                        </div>
+                    </div>
+                
+                    <?php if ( has_post_thumbnail() ) { 
+
+                        $attachment_id = get_post_thumbnail_id();
+                        $attachment_data = wp_prepare_attachment_for_js( $attachment_id );
+
+                        the_post_thumbnail( 
+                            'dmc-slider', 
+                            array( 
+                                'class' => 'hero',
+                                'alt' => trim(strip_tags( $attachment_data['alt'] ) ),
+                                'title'   => trim(strip_tags( $attachment_data['title'] )),
+                            )
+                        ); 
+                    }; ?>
+                </a>
+            </div>
+
+            <?php endforeach; 
+        endif; 
+
+    endwhile; wp_reset_postdata();
+}
+
+// disable default dashboard widgets
+function disable_default_dashboard_widgets() {
+    // remove_meta_box('dashboard_right_now', 'dashboard', 'core');
+    //QuickPress
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'core');
+    //Wordpress Development Blog Feed
+    remove_meta_box('dashboard_primary', 'dashboard', 'core');
+    //Other Wordpress News Feed
+    remove_meta_box('dashboard_secondary', 'dashboard', 'core');
+    //Plugins - Popular, New and Recently updated Wordpress Plugins
+    remove_meta_box('dashboard_plugins', 'dashboard', 'core');
+    // Recent Comments
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'core');
+    // remove_meta_box('dashboard_incoming_links', 'dashboard', 'core');
+    // remove_meta_box('dashboard_recent_drafts', 'dashboard', 'core'); 
+}
+add_action('admin_menu', 'disable_default_dashboard_widgets');
+
+//Giving Editors Access to Gravity Forms and Appearance menu
+    function dmc_gforms_access(){
+        $role = get_role('editor');
+        $role->add_cap('gform_full_access');
+        $role->add_cap( 'edit_theme_options' );
+    }
+add_action('admin_init','dmc_gforms_access');
+
+// Check if page is a child
+function is_tree( $pid ) {      
+    global $post;               
+    if ( is_page($pid) )
+        return true;            
+    $anc = get_post_ancestors( $post->ID );
+    foreach ( $anc as $ancestor ) {
+        if( is_page() && $ancestor == $pid ) {
+            return true;
+        }
+    }
+    return false; 
+}
+
+// check is post is a custom post type http://wordpress.stackexchange.com/questions/6731/if-is-custom-post-type
+function is_post_type($type){
+    global $wp_query;
+    if($type == get_post_type($wp_query->post->ID)) return true;
+    return false;
+}
+
+function dmc_custom_login_logo_url() {
+    return get_bloginfo( 'url' );
+}
+add_filter( 'login_headerurl', 'dmc_custom_login_logo_url' );
+
+function dmc_custom_login_logo_url_title() {
+    return get_bloginfo( 'name' );
+}
+add_filter( 'login_headertitle', 'dmc_custom_login_logo_url_title' );
+
+function dmc_custom_login_logo() { ?>
+    <style type="text/css">
+    	body.login div#login{
+    		padding-top: 70px;
+    	}
+        body.login div#login h1 a {
+        	width: 320px;
+        	height: 129px;
+        	margin-left: 4px;
+            background-image: url(<?php echo get_bloginfo( 'template_directory' ) ?>/img/logo-med.png);
+            background-size: 320px 129px;
+            padding-bottom: 30px;
+        }
+    </style>
+<?php }
+add_action( 'login_enqueue_scripts', 'dmc_custom_login_logo' );
+
+
+
