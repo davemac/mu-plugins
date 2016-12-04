7ca35976c8344bb05641976af2ff69fd1c8c3149
David McDonald <info@davidmcdonald.org>
2016-12-05 10:48:33 +1100

    adds plugin to enable editors to manage users

--- load.php
+++ load.php
@@ -2,3 +2,4 @@
 require WPMU_PLUGIN_DIR . '/remove-wp-menu/remove-wp-menu.php';
 require WPMU_PLUGIN_DIR . '/dmc-helpers/dmc-helpers.php';
 require WPMU_PLUGIN_DIR . '/user-switching/user-switching.php';
+require WPMU_PLUGIN_DIR . '/dmc-editors-manage-users.php';
