56735cc27bd551b84a3974d4c4572c13f2a6df51
David McDonald <info@davidmcdonald.org>
2020-02-17 12:25:13 +1100

    removes user-switching

--- load.php
+++ load.php
@@ -1,5 +1,4 @@
 <?php // mu-plugins/load.php
 require WPMU_PLUGIN_DIR . '/remove-wp-menu/remove-wp-menu.php';
 require WPMU_PLUGIN_DIR . '/dmc-helpers/dmc-helpers.php';
-require WPMU_PLUGIN_DIR . '/user-switching/user-switching.php';
 require WPMU_PLUGIN_DIR . '/dmc-editors-manage-users/class-dmc-editors-manage-users.php';
