151afd9d1794e792ac6494863d26ff9740a08e7e
David McDonald <info@davidmcdonald.org>
2023-05-23 10:41:42 +1000

    removes redundant plugin

--- load.php
+++ load.php
@@ -1,4 +1,3 @@
 <?php // mu-plugins/load.php
 require WPMU_PLUGIN_DIR . '/remove-wp-menu/remove-wp-menu.php';
 require WPMU_PLUGIN_DIR . '/dmc-helpers/dmc-helpers.php';
-require WPMU_PLUGIN_DIR . '/dmc-editors-manage-users/class-dmc-editors-manage-users.php';
